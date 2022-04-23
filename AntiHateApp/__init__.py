e8c73d4abe4cb90b3ad34c00804ec879d26c3541
Daniel Breiner <contact@danielbreiner.eu>
2022-04-23 15:11:40 +0200

    Added CORS support

--- AntiHateApp/__init__.py
+++ AntiHateApp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flasgger import Swagger
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 # for swagger openApi
@@ -8,6 +9,7 @@ swagger = Swagger(app)
 # for sqlite DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/database.db'
 db = SQLAlchemy(app) 
+cors = CORS(app)
 
 def register_blueprints():
     from AntiHateApp.controllers.api import api
